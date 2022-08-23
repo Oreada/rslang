2196fd1329440f332b86391c690ed1c93b2ad614
Klychnikov Alexandr <klychnikov75@mail.ru>
2022-08-23 23:50:23 +0300

    feat: implement create user from register form and login registered user with receiving token

--- src/components/modalWindow/logonListener.ts
+++ src/components/modalWindow/logonListener.ts
@@ -1,19 +1,27 @@
 export const listenLogon = () => {
     const logon = document.querySelector('.auth');
     const modal = document.querySelector('.overlay') as HTMLElement;
+    const loginEmail = document.getElementById('email-login') as HTMLInputElement;
+    const loginPassword = document.getElementById('password-login') as HTMLInputElement;
+    const message = document.querySelector('.message__incorrect') as HTMLElement;
     logon?.addEventListener('click', () => {
         modal.style.display = 'flex';
     });
     window.addEventListener('keydown', function (e) {
         if (e.key == 'Escape') {
             modal.style.display = 'none';
+            loginEmail.value = '';
+            loginPassword.value = '';
+            message.style.display = 'none';
         }
     });
     modal.addEventListener('click', function (e) {
         const element = e.target as HTMLElement;
-        console.log(element);
         if (!element.closest('.form__container')) {
             modal.style.display = 'none';
+            loginEmail.value = '';
+            loginPassword.value = '';
+            message.style.display = 'none';
         }
     });
 };
