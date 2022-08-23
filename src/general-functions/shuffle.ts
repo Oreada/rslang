4305dcc58b3e068fcdd48ce3484fd0969800046f
Olga Antushevich <antushevich@gmail.com>
2022-08-23 12:47:04 +0300

    feat: add function shuffle

---
+++ src/general-functions/shuffle.ts
@@ -0,0 +1,7 @@
+//! перемешивание элементов массива
+export function shuffle(array: Array<string>) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
