1eda4d74f48307310f04106ab1f168be499d048a
Olga Antushevich <antushevich@gmail.com>
2022-08-31 13:33:31 +0300

    fix: fix the function processAudiochallengeResults

--- src/components/games-results-of-games/process-audiochallenge-results.ts
+++ src/components/games-results-of-games/process-audiochallenge-results.ts
@@ -20,10 +20,12 @@ export async function processAudiochallengeResults(
                         totalIncorrectAudiochallenge: pair[1] === 'incorrect' ? 1 : 0,
                         totalCorrectSprint: 0,
                         totalIncorrectSprint: 0,
-                        consecutiveCorrectAudiochallenge: pair[1] === 'correct' ? 1 : 0,
-                        consecutiveIncorrectAudiochallenge: pair[1] === 'incorrect' ? 1 : 0,
-                        consecutiveCorrectSprint: 0,
-                        consecutiveIncorrectSprint: 0,
+                        consecutiveCorrectAudiochallenge: pair[1] === 'correct' ? 1 : 0, //! - не используем - ?
+                        consecutiveIncorrectAudiochallenge: pair[1] === 'incorrect' ? 1 : 0, //! - не используем - ?
+                        consecutiveCorrectSprint: 0, //! - не используем - ?
+                        consecutiveIncorrectSprint: 0, //! - не используем - ?
+                        consecutiveCorrectAll: pair[1] === 'correct' ? 1 : 0,
+                        consecutiveIncorrectAll: pair[1] === 'incorrect' ? 1 : 0,
                     },
                 },
                 tokenUser
@@ -33,24 +35,32 @@ export async function processAudiochallengeResults(
 
             const oldTotalCorrect = word.optional.totalCorrectAudiochallenge;
             const oldTotalIncorrect = word.optional.totalIncorrectAudiochallenge;
-            const oldConsecutiveCorrect = word.optional.consecutiveCorrectAudiochallenge;
-            const oldConsecutiveIncorrect = word.optional.consecutiveIncorrectAudiochallenge;
+            const oldConsecutiveCorrectAudiochallenge = word.optional.consecutiveCorrectAudiochallenge; //! - не используем - ?
+            const oldConsecutiveIncorrectAudiochallenge = word.optional.consecutiveIncorrectAudiochallenge; //! - не используем - ?
+            const oldConsecutiveCorrect = word.optional.consecutiveCorrectAll;
+            const oldConsecutiveIncorrect = word.optional.consecutiveIncorrectAll;
 
             console.log(pair[0], oldTotalCorrect, oldTotalIncorrect, oldConsecutiveCorrect, oldConsecutiveIncorrect);
 
             let newDifficulty = word.difficulty;
             let newTotalCorrect = oldTotalCorrect;
             let newTotalIncorrect = oldTotalIncorrect;
+            let newConsecutiveCorrectAudiochallenge = oldConsecutiveCorrectAudiochallenge; //! - не используем - ?
+            let newConsecutiveIncorrectAudiochallenge = oldConsecutiveIncorrectAudiochallenge; //! - не используем - ?
             let newConsecutiveCorrect = oldConsecutiveCorrect;
             let newConsecutiveIncorrect = oldConsecutiveIncorrect;
 
             if (pair[1] === 'correct') {
                 newTotalCorrect = oldTotalCorrect + 1;
+                newConsecutiveCorrectAudiochallenge = oldConsecutiveCorrectAudiochallenge + 1; //! - не используем - ?
                 newConsecutiveCorrect = oldConsecutiveCorrect + 1;
+                newConsecutiveIncorrectAudiochallenge = 0; //! - не используем - ?
                 newConsecutiveIncorrect = 0; //! обнуляем серию неправильных ответов подряд после верного ответа
             } else if (pair[1] === 'incorrect') {
                 newTotalIncorrect = oldTotalIncorrect + 1;
+                newConsecutiveIncorrectAudiochallenge = oldConsecutiveIncorrectAudiochallenge + 1; //! - не используем - ?
                 newConsecutiveIncorrect = oldConsecutiveIncorrect + 1;
+                newConsecutiveCorrectAudiochallenge = 0; //! - не используем - ?
                 newConsecutiveCorrect = 0; //! обнуляем серию правильных ответов подряд после неверного ответа
 
                 if (word.difficulty === 'easy') {
@@ -82,10 +92,12 @@ export async function processAudiochallengeResults(
                         totalIncorrectAudiochallenge: newTotalIncorrect as number,
                         totalCorrectSprint: word.optional.totalCorrectSprint,
                         totalIncorrectSprint: word.optional.totalIncorrectSprint,
-                        consecutiveCorrectAudiochallenge: newConsecutiveCorrect as number,
-                        consecutiveIncorrectAudiochallenge: newConsecutiveIncorrect as number,
-                        consecutiveCorrectSprint: word.optional.consecutiveCorrectSprint,
-                        consecutiveIncorrectSprint: word.optional.consecutiveIncorrectSprint,
+                        consecutiveCorrectAudiochallenge: newConsecutiveCorrectAudiochallenge as number, //! - не используем - ?
+                        consecutiveIncorrectAudiochallenge: newConsecutiveIncorrectAudiochallenge as number, //! - не используем - ?
+                        consecutiveCorrectSprint: word.optional.consecutiveCorrectSprint, //! - не используем - ?
+                        consecutiveIncorrectSprint: word.optional.consecutiveIncorrectSprint, //! - не используем - ?
+                        consecutiveCorrectAll: newConsecutiveCorrect as number,
+                        consecutiveIncorrectAll: newConsecutiveIncorrect as number,
                     },
                 },
                 tokenUser
