81724fee8362597c4dc570d2ea4d856b047129bb
Klychnikov Alexandr <klychnikov75@mail.ru>
2022-09-02 12:38:35 +0300

    feat: add modal window for sprint and change disign for a little

--- src/components/game-sprint/gameSprintListeners.ts
+++ src/components/game-sprint/gameSprintListeners.ts
@@ -1,101 +1,116 @@
-import { sprintStorage } from "../../storage/storage";
-import { IWord, _IWord } from "../../types/types";
-import { getPageForSprint, getSprintWords, getSprintWordsFromPage, getStartSprintWords, resetSprintProgress, sprintTimer, updateSprintProgressRender, updateSprintProgressState } from "./gameSprintFunctions";
-import { renderSprintGame } from "./renderSprintGame";
+import { sprintStorage } from '../../storage/storage';
+import { IWord, _IWord } from '../../types/types';
+import {
+    getPageForSprint,
+    getSprintWords,
+    getSprintWordsFromPage,
+    getStartSprintWords,
+    resetSprintProgress,
+    sprintTimer,
+    updateSprintProgressRender,
+    updateSprintProgressState,
+} from './gameSprintFunctions';
+import { renderSprintGame } from './renderSprintGame';
 
 export function addStartSprintListener() {
-  const target = document.querySelector('.sprint-start-button') as HTMLButtonElement;
-  const start = document.querySelector('.sprint-start') as HTMLButtonElement;
-  const game = document.querySelector('.sprint-game-container') as HTMLElement;
-  const gameMain = document.querySelector('.sprint-main') as HTMLElement;
-  if (!target || !game || !start || !gameMain) {
-    return;
-  }
+    const target = document.querySelector('.sprint-start-button') as HTMLButtonElement;
+    const start = document.querySelector('.sprint-start') as HTMLButtonElement;
+    const game = document.querySelector('.sprint-game-container') as HTMLElement;
+    const gameMain = document.querySelector('.sprint-main') as HTMLElement;
+    if (!target || !game || !start || !gameMain) {
+        return;
+    }
 
-  target.addEventListener('click', async () => {
-    await getPageForSprint();
-    await getSprintWordsFromPage();
-    gameMain.innerHTML = renderSprintGame();
-    sprintTimer();
-    game.classList.remove('hidden');
-    start.classList.add('hidden')
-  })
+    target.addEventListener('click', async () => {
+        await getPageForSprint();
+        await getSprintWordsFromPage();
+        gameMain.innerHTML = renderSprintGame();
+        sprintTimer();
+        game.classList.remove('hidden');
+        start.classList.add('hidden');
+    });
 }
 
 export function addSprintListener() {
-  const target = document.querySelector('.sprint-button') as HTMLButtonElement;
-  const game = document.querySelector('.sprint-game-container') as HTMLElement;
-  if (!target || !game) {
-    return;
-  }
+    const target = document.querySelector('.sprint-button') as HTMLButtonElement;
+    const game = document.querySelector('.sprint-game-container') as HTMLElement;
+    if (!target || !game) {
+        return;
+    }
 
-  target.addEventListener('click', async () => {
-    console.log('aaa')
-  })
+    target.addEventListener('click', async () => {
+        console.log('aaa');
+    });
 }
 
 export function addGameButtonListener() {
-  const target = document.querySelectorAll('.sprint-button') as NodeListOf<HTMLButtonElement>;
-  const game = document.querySelector('.sprint-main') as HTMLElement;
-  if (!target || !game) {
-    return;
-  }
+    const target = document.querySelectorAll('.sprint-button') as NodeListOf<HTMLButtonElement>;
+    const game = document.querySelector('.sprint-main') as HTMLElement;
+    if (!target || !game) {
+        return;
+    }
 
-  target.forEach((item) => {
-    item.addEventListener('click', async () => {
+    target.forEach((item) => {
+        item.addEventListener('click', async () => {
+            const id1 = (sprintStorage.originWord as _IWord)._id;
+            const id2 = (sprintStorage.translateWord as _IWord)._id;
 
-      const id1 = (sprintStorage.originWord as _IWord)._id;
-      const id2 = (sprintStorage.translateWord as _IWord)._id;
+            const isCorrect: string = (id1 === id2).toString();
 
-      const isCorrect: string = (id1 === id2).toString();
-  
-      if (isCorrect === item.dataset.correctly) {
-        sprintStorage.currentScore += sprintStorage.scoreDecrease;
-        sprintStorage.levelProgress += 1;
-        sprintStorage.gameResult[id1] = 'correct';
-      } else {
-        resetSprintProgress();
-        sprintStorage.gameResult[id1] = 'incorrect';
-      }
+            if (isCorrect === item.dataset.correctly) {
+                sprintStorage.currentScore += sprintStorage.scoreDecrease;
+                sprintStorage.levelProgress += 1;
+                sprintStorage.gameResult[id1] = 'correct';
+            } else {
+                resetSprintProgress();
+                sprintStorage.gameResult[id1] = 'incorrect';
+            }
 
-      await getSprintWordsFromPage();
+            await getSprintWordsFromPage();
 
-      console.log(sprintStorage.gameSource);
-      console.log(sprintStorage.originWord?.word, sprintStorage.translateWord?.word, sprintStorage.currentPageWords?.length)
+            console.log(sprintStorage.gameSource);
+            console.log(
+                sprintStorage.originWord?.word,
+                sprintStorage.translateWord?.word,
+                sprintStorage.currentPageWords?.length
+            );
 
-      updateSprintProgressState();
+            updateSprintProgressState();
 
-      game.innerHTML = renderSprintGame();
-      updateSprintProgressRender();
-    })
-  })
+            game.innerHTML = renderSprintGame();
+            updateSprintProgressRender();
+        });
+    });
 }
 
 export function addEndSprintListener() {
-  const start = document.querySelector('.sprint-start') as HTMLButtonElement;
-  const result = document.querySelector('.sprint-result-container') as HTMLElement;
-  if (!result || !start) {
-    return;
-  }
-
-  result.addEventListener('click', (event: Event) => {
-    if ((event.target as HTMLButtonElement).classList.contains('results__end-btn')) {
-      start.classList.remove('hidden')
-      result.classList.add('hidden');
+    const start = document.querySelector('.sprint-start') as HTMLButtonElement;
+    const result = document.querySelector('.sprint-result-container') as HTMLElement;
+    if (!result || !start) {
+        return;
     }
-  })
+
+    result.addEventListener('click', (event: Event) => {
+        if (
+            (event.target as HTMLButtonElement).classList.contains('results__end-btn') ||
+            (event.target as HTMLButtonElement).classList.contains('sprint-result-button')
+        ) {
+            start.classList.remove('hidden');
+            result.classList.add('hidden');
+        }
+    });
 }
 
 export function addSprintOptionListener() {
-  const target = document.querySelectorAll('.sprint-group-option') as NodeListOf<HTMLOptionElement>;
-  if (!target) {
-    return;
-  }
+    const target = document.querySelectorAll('.sprint-group-option') as NodeListOf<HTMLOptionElement>;
+    if (!target) {
+        return;
+    }
 
-  target.forEach((item) => {
-    item.addEventListener('click', () => {
-      sprintStorage.currentChapter = item.value;
-      sprintStorage.currentPage = '0';
-    })
-  })
+    target.forEach((item) => {
+        item.addEventListener('click', () => {
+            sprintStorage.currentChapter = item.value;
+            sprintStorage.currentPage = '0';
+        });
+    });
 }
--- src/components/modalWindow/modal.ts
+++ src/components/modalWindow/modal.ts
@@ -4,7 +4,7 @@ export const Modal = (content = ''): string => {
     return `
     <div class="modal">
       <div class="modal-content">
-        <button class="close" tabindex="-1"><span class="material-icons" onclick="history.go(-2); this.closest('.modal').style='display: none;'">close</span></button>
+        <button class="close" tabindex="-1"><div class="material-icons" onclick="history.go(-2); this.closest('.modal').style='display: none;'"><img src="../../asset/svg/close.svg" alt =""></div></button>
         ${content}
       </div>
     </div>
