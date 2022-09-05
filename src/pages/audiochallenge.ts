3c6f9532d3673287236fc997af94862e7f79053d
Olga Antushevich <antushevich@gmail.com>
2022-09-05 23:43:30 +0300

    fix: change links to sounds

--- src/pages/audiochallenge.ts
+++ src/pages/audiochallenge.ts
@@ -77,7 +77,7 @@ export const AudiochallengeCallback = () => {
     ) as NodeListOf<HTMLButtonElement>;
 
     setTimeout(() => {
-        const path = `${BASE_API}${questionAudioButtons[0].dataset.audiopath as string}`;
+        const path = `${BASE_API}/${questionAudioButtons[0].dataset.audiopath as string}`;
         const audio = new Audio(path);
         // try {
         //     (audio as HTMLAudioElement).play();
