e99decf7c1ec2d7e39782c8dfca2a4b05bc6a4b4
Olga Antushevich <antushevich@gmail.com>
2022-09-05 20:06:36 +0300

    fix: change paths to images for deploy

--- src/pages/common/footer.ts
+++ src/pages/common/footer.ts
@@ -1,10 +1,10 @@
 import './footer.sass';
 
 export const Footer = (): string => {
-    const Footer = `<footer class="footer">
+  const Footer = `<footer class="footer">
     <div class="footer__wrapper">
       <div class="personal">
-        <img class="github-logo" src="../asset/github-logo.png" alt="" />
+        <img class="github-logo" src="asset/github-logo.png" alt="github-logo"/>
         <div class="developers">
           <a href="https://github.com/Oreada/" class="footer_link github">Olga Antushevich</a>
           <a href="https://github.com/iliyakz" class="footer_link github">Iliya Kuznetsov</a>
@@ -14,10 +14,10 @@ export const Footer = (): string => {
       <div class="rss">
         <span class="year">2022'</span>
         <a href="https://rs.school/js/" class="footer_link rss">
-          <img class="rss-logo" src="../asset/rs_school_js.svg" alt=""/>
+          <img class="rss-logo" src="asset/rs_school_js.svg" alt="rss-logo"/>
         </a>
       </div>
     </div>  
     </footer>`;
-    return Footer;
+  return Footer;
 };
--- src/pages/games.ts
+++ src/pages/games.ts
@@ -5,12 +5,12 @@ export const GamesContent = () => {
     return `<section class="game__area">
         <div class="game__box" data-rout="/rslang/games/audiochallenge">
           <p class="game__description">Тренировка Аудиовызов улучшает твое восприятие речи на слух.</p>
-          <img class="game-icon" src="../asset/svg/volume_up.svg" alt="">
+          <img class="game-icon" src="asset/svg/volume_up.svg" alt="">
           <a href="" class="nav__link game" data-rout="/rslang/games/audiochallenge">Audiochallenge</a>
         </div>
         <div class="game__box" data-rout="/rslang/games/sprint">
           <p class="game__description">Спринт - тренировка на скорость. Попробуй угадать как можно больше слов за 30 секунд.</p>
-          <img class="game-icon" src="../asset/svg/timer.svg" alt="">
+          <img class="game-icon" src="asset/svg/timer.svg" alt="">
           <a href="" class="nav__link game" data-rout="/rslang/games/sprint">Sprint</a>
         </div>    
     </section>`;
